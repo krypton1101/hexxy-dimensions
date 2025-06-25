0fc13f51bf547bfbd824648a600b8ffafb804186
krypton1101 <krypton11@mail.ru>
2025-06-25 21:41:37 +0300

    Revert "Hexal mimicry (didn't work)"
    This reverts commit ea9d34761c4933d7d7e047aef0d06d08c4b56b73.

--- src/main/java/net/walksanator/hexdim/HexxyDimensions.kt
+++ src/main/java/net/walksanator/hexdim/HexxyDimensions.kt
@@ -2,7 +2,6 @@ package net.walksanator.hexdim
 
 import at.petrak.hexcasting.api.item.IotaHolderItem
 import at.petrak.hexcasting.common.items.magic.ItemMediaBattery
-import at.petrak.hexcasting.common.lib.hex.HexActions
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParseException
 import com.mojang.brigadier.arguments.IntegerArgumentType.getInteger
@@ -37,7 +36,6 @@ import net.walksanator.hexdim.util.Rectangle
 import org.slf4j.LoggerFactory
 import java.nio.file.StandardOpenOption
 import java.util.*
-import java.util.function.BiConsumer
 import kotlin.io.path.exists
 import kotlin.io.path.reader
 import kotlin.io.path.writeText
@@ -63,7 +61,7 @@ object HexxyDimensions : ModInitializer {
         logger.info("Hello Fabric world!")
 
         IotaTypes.registerTypes()
-        DimPatternRegistry.registerPatterns(bind(HexActions.REGISTRY))
+        DimPatternRegistry.registerPatterns()
 
 
         Registry.register(Registries.BLOCK, Identifier(MOD_ID, "skybox"), BlockRegistry.SKYBOX)
@@ -364,14 +362,6 @@ object HexxyDimensions : ModInitializer {
                 )
             }
         }
-    }
-
-    private fun <T> bind(registry: Registry<in T>): BiConsumer<T, Identifier> =
-        BiConsumer<T, Identifier> { t, id -> Registry.register(registry, id, t) }
-
-
-    fun modLoc(s: String): Identifier {
-        return Identifier(MOD_ID, s);
     }
 }
 
--- src/main/java/net/walksanator/hexdim/patterns/DimPatternRegistry.kt
+++ src/main/java/net/walksanator/hexdim/patterns/DimPatternRegistry.kt
@@ -2,55 +2,46 @@ package net.walksanator.hexdim.patterns
 
 import at.petrak.hexcasting.api.casting.ActionRegistryEntry
 import at.petrak.hexcasting.api.casting.castables.Action
-import at.petrak.hexcasting.api.casting.castables.OperationAction
 import at.petrak.hexcasting.api.casting.math.HexDir
 import at.petrak.hexcasting.api.casting.math.HexPattern
+import at.petrak.hexcasting.common.lib.hex.HexActions
+import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
+import net.walksanator.hexdim.HexxyDimensions
 import net.walksanator.hexdim.patterns.dim.*
-import net.walksanator.hexdim.HexxyDimensions.modLoc
 import java.util.function.BiConsumer
 
 
 object DimPatternRegistry {
 
-    private val ACTIONS: MutableMap<Identifier, ActionRegistryEntry> = mutableMapOf()
+    private val ACTIONS: MutableMap<Identifier, ActionRegistryEntry> =
+        LinkedHashMap()
 
-    @JvmStatic
-    fun registerPatterns(r: BiConsumer<ActionRegistryEntry, Identifier>) {
-        for ((key, value) in ACTIONS.entries) {
+    val DIM_CREATE = pattern("wawdwawawdwawawdwewdwqwdwqwdwqwdwqwdwqwdw",HexDir.SOUTH_WEST,"dim/create",OpCreateDimension()) //
+    val DIM_KIDNAP = pattern("wawewawewawewawewawewawwwqwqwqwqwqwaeqqqqqaww",HexDir.SOUTH_WEST,"dim/kidnap",OpKidnap())
+    val DIM_BANISH = pattern("wwdeeeeeqdwewewewewewwwdwqwdwqwdwqwdwqwdwqwdw", HexDir.EAST, "dim/kick",OpBanish()) //
+    val DIM_KEY = pattern("awqwawqdqawwwaq",HexDir.SOUTH_EAST,"dim/pos/set",OpDimSetPos()) //
+    val DIM_POSKEY = pattern("dewedaewdwewd",HexDir.EAST,"dim/perm/remove",OpDimStripPermission()) //
+    val DIM_TORELATIVE = pattern("adeeda",HexDir.EAST,"dim/rel/to",OpDimRelative(true)) //
+    val DIM_FROMRELATIVE = pattern("daqqad",HexDir.NORTH_EAST,"dim/rel/from",OpDimRelative(false)) //
+    val DIM_ACTIVATE = pattern("deaqqeweeeeewdqdqdqdqdq", HexDir.SOUTH_EAST,"dim/cast/activate", OpDimExecute(true)) //
+    val DIM_DEACTIVATE = pattern("aqdeeqeaeaeaeaeae",HexDir.SOUTH_WEST, "dim/cast/deactivate", OpDimExecute(false)) //
+    val DIM_CARVED = pattern("qqqqqwaeaeaeaeaeadwaqaeaq", HexDir.NORTH_WEST, "dim/carved",OpDimCarved()) //
+    val DIM_ESTIMATE_TIME = pattern("qqqqqwaeaeaeaeaeadqwdwqwdwdwqw", HexDir.NORTH_EAST, "dim/time",OpEstimateTime())
+
+    fun registerPatterns() {
+        val r = BiConsumer { type: ActionRegistryEntry, id: Identifier -> Registry.register(HexActions.REGISTRY, id, type) }
+        for ((key, value) in ACTIONS) {
             r.accept(value, key)
         }
     }
 
-    val DIM_CREATE = make("dim/create", HexPattern.fromAngles("wawdwawawdwawawdwewdwqwdwqwdwqwdwqwdwqwdw", HexDir.SOUTH_WEST), OpCreateDimension()) //
-    val DIM_KIDNAP = make("dim/kidnap", HexPattern.fromAngles("wawewawewawewawewawewawwwqwqwqwqwqwaeqqqqqaww", HexDir.SOUTH_WEST), OpKidnap())
-    val DIM_BANISH = make("dim/kick", HexPattern.fromAngles("wwdeeeeeqdwewewewewewwwdwqwdwqwdwqwdwqwdwqwdw", HexDir.EAST), OpBanish()) //
-    val DIM_KEY = make("dim/pos/set", HexPattern.fromAngles("awqwawqdqawwwaq", HexDir.SOUTH_EAST), OpDimSetPos()) //
-    val DIM_POSKEY = make("dim/perm/remove", HexPattern.fromAngles("dewedaewdwewd", HexDir.EAST), OpDimStripPermission()) //
-    val DIM_TORELATIVE = make("dim/rel/to", HexPattern.fromAngles("adeeda", HexDir.EAST), OpDimRelative(true)) //
-    val DIM_FROMRELATIVE = make("dim/rel/from", HexPattern.fromAngles("daqqad", HexDir.NORTH_EAST), OpDimRelative(false)) //
-    val DIM_ACTIVATE = make("dim/cast/activate", HexPattern.fromAngles("deaqqeweeeeewdqdqdqdqdq", HexDir.SOUTH_EAST), OpDimExecute(true)) //
-    val DIM_DEACTIVATE = make("dim/cast/deactivate", HexPattern.fromAngles("aqdeeqeaeaeaeaeae", HexDir.SOUTH_WEST), OpDimExecute(false)) //
-    val DIM_CARVED = make("dim/carved", HexPattern.fromAngles("qqqqqwaeaeaeaeaeadwaqaeaq", HexDir.NORTH_WEST), OpDimCarved()) //
-    val DIM_ESTIMATE_TIME = make("dim/time", HexPattern.fromAngles("qqqqqwaeaeaeaeaeadqwdwqwdwdwqw", HexDir.NORTH_EAST), OpEstimateTime())
-
-
-    fun make(name: String, pattern: HexPattern, action: Action): ActionRegistryEntry = make(name, ActionRegistryEntry(pattern, action))
-
-    fun make(name: String, are: ActionRegistryEntry): ActionRegistryEntry {
-        return if (ACTIONS.put(modLoc(name), are) != null) {
+    private fun pattern(pat: String, dir: HexDir, name: String, oa: Action): ActionRegistryEntry {
+        val are = ActionRegistryEntry(HexPattern.fromAngles(pat,dir),oa)
+        val old = ACTIONS.put(Identifier(HexxyDimensions.MOD_ID,name), are)
+        if (old != null) {
             throw IllegalArgumentException("Typo? Duplicate id $name")
-        } else {
-            are
         }
-    }
-
-    fun make(name: String, oa: OperationAction): ActionRegistryEntry {
-        val are = ActionRegistryEntry(oa.pattern, oa)
-        return if (ACTIONS.put(modLoc(name), are) != null) {
-            throw IllegalArgumentException("Typo? Duplicate id $name")
-        } else {
-            are
-        }
+        return are
     }
 }
