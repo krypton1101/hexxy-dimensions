8d5e35fbd9c742e8f71b32a544d129e0f1a376c6
krypton1101 <krypton11@mail.ru>
2025-06-25 14:29:04 +0300

    OpKidnap changed to SpellAction

--- src/main/java/net/walksanator/hexdim/patterns/dim/OpKidnap.kt
+++ src/main/java/net/walksanator/hexdim/patterns/dim/OpKidnap.kt
@@ -1,5 +1,7 @@
 package net.walksanator.hexdim.patterns.dim
 
+import at.petrak.hexcasting.api.casting.RenderedSpell
+import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.EntityIota
 import at.petrak.hexcasting.api.casting.iota.Iota
@@ -13,16 +15,16 @@ import net.minecraft.text.Text
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.TeleportTarget
 import net.walksanator.hexdim.HexxyDimensions
-import net.walksanator.hexdim.casting.VarMediaOutputAction
 import net.walksanator.hexdim.iotas.RoomIota
 import net.walksanator.hexdim.casting.mishap.MishapInvalidRoom
 
+class OpKidnap : SpellAction {
+    override val argc: Int
+        get() = 2
 
-class OpKidnap : VarMediaOutputAction {
-    override val argc = 2
-    override fun execute(args: List<Iota>, env: CastingEnvironment): VarMediaOutputAction.CastResult {
-        val room = args[0]
+    override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
         val mediacost: Long
+        val room = args[0]
         if (room !is RoomIota) {throw MishapInvalidIota(room,1,Text.translatable("hexdim.iota.roomlike"))}
         if (!(room).permissions[0]) {throw MishapInvalidIota(room,1, Text.translatable("hexdim.iota.permissions.read"))}
         if (!room.getRoomValue().isDone) {throw MishapInvalidRoom(room.getRoomValue(), false)}
@@ -30,7 +32,11 @@ class OpKidnap : VarMediaOutputAction {
 
         val target: Either<EntityIota,ListIota> = when (iota.type) {
             ListIota.TYPE -> {
-                mediacost = MediaConstants.SHARD_UNIT * (iota as ListIota).list.size()
+                val iotas = (iota as ListIota).list.filter { value -> value.type == EntityIota.TYPE }
+                if (iotas.isEmpty()) {
+                    throw MishapInvalidIota(iota, 1, Text.literal("List contains no entities"))
+                }
+                mediacost = MediaConstants.SHARD_UNIT * iota.list.size()
                 Either.right(iota)
             }
             EntityIota.TYPE -> {
@@ -41,16 +47,18 @@ class OpKidnap : VarMediaOutputAction {
                 throw MishapInvalidIota(iota, 1, Text.literal("Iota is not a list of entities or entity"))
             }
         }
-        return Spell(mediacost,room,target)
+
+        return SpellAction.Result(
+            Spell(room,target),
+            mediacost,
+            listOf()
+        )
     }
 
-    class Spell(cost: Long, val room: RoomIota,val targets: Either<EntityIota, ListIota>) : VarMediaOutputAction.CastResult(cost) {
-        override fun run(env: CastingEnvironment): List<Iota> {
-            targets.ifRight {iota ->
+    private data class Spell(val room: RoomIota, val targets: Either<EntityIota, ListIota>) : RenderedSpell{
+        override fun cast(env: CastingEnvironment) {
+            targets.ifRight { iota ->
                 val iotas = iota.list.filter { value -> value.type == EntityIota.TYPE }
-                if (iotas.isEmpty()) {
-                    throw MishapInvalidIota(iota, 1, Text.literal("List contains no entities"))
-                }
                 for (entity in iotas) {
                     val target = (entity as EntityIota).entity
                     env.assertEntityInRange(target)
@@ -62,9 +70,7 @@ class OpKidnap : VarMediaOutputAction {
                 env.assertEntityInRange(target)
                 kidnap(room.getTeleportPosition(), target)
             }
-            return listOf()
         }
-
     }
 }
 
